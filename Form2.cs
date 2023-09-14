17443b796565e04eda568cd0bc2184dd0cdc5bce
rkrehn <satsui@msn.com>
2023-09-14 15:42:41 -0600

    Update Form2.cs
    Updated the APIKey to save in a file rather than properties. Each time Outlook updated, the properties would clear it.
    
    Signed-off-by: rkrehn <satsui@msn.com>

--- Form2.cs
+++ Form2.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,11 @@ namespace OutlookGPT
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            textBox1.Text = Properties.Settings.Default.OpenAPI;
+            if (!File.Exists(System.Windows.Forms.Application.UserAppDataPath + "openaikey.dat")) return;
+            using (StreamReader sr = new StreamReader(System.Windows.Forms.Application.UserAppDataPath + "openaikey.dat"))
+            {
+                textBox1.Text = sr.ReadToEnd();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,8 +36,13 @@ namespace OutlookGPT
                 return;
             } else
             {
-                Properties.Settings.Default.OpenAPI = textBox1.Text;
-                Properties.Settings.Default.Save();
+                using(StreamWriter sw = new StreamWriter(Application.UserAppDataPath + "openaikey.dat"))
+                {
+                    sw.WriteLine(textBox1.Text);
+                }
+
+                Form frm1 = new Form1();
+                frm1.ShowDialog();
                 this.Hide();
             }
 
